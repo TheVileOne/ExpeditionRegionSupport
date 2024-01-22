ce57cea8200180cd694c9e9dba79b2c842dafab2
Fluffball <xeolite@yahoo.com>
2024-01-22 17:43:51 -0500

    New Dialog UI classes

---
+++ RandomStartsFix/Interface/FilterOptions.cs
@@ -0,0 +1,204 @@
+﻿using HarmonyLib;
+using Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ExpeditionRegionSupport.Interface
+{
+    public class FilterOptions : PositionedMenuObject, CheckBox.IOwnCheckBox
+    {
+        public const int DEFAULT_CHALLENGE_COUNT = 9;
+
+        //public CheckBox.IOwnCheckBox OptionHandle;
+
+        public List<FSprite> Dividers = new List<FSprite>();
+        public List<MenuLabel> Filters = new List<MenuLabel>();
+        public List<FilterCheckBox> Options = new List<FilterCheckBox>();
+
+        /// <summary>
+        /// Used to keep last divider from being rendered
+        /// </summary>
+        private FSprite pendingDivider;
+
+        /// <summary>
+        /// A counter that disables input handling when user clicks too soon
+        /// </summary>
+        public int DoubleClickProtection;
+
+        /// <summary>
+        /// A flag that indicates that user has clicked more than once in a very short period of time
+        /// </summary>
+        public bool DoubleClick;
+
+        public CheckBox LastFilterChanged;
+
+        private bool filtersDirty;
+
+        public FilterOptions(Menu.Menu menu, MenuObject owner, Vector2 pos) : base(menu, owner, pos)
+        {
+        }
+
+        public void AddOption(FilterCheckBox option)
+        {
+            Options.Add(option);
+            Filters.Add(option.label);
+
+            FSprite divider = createDividerSprite();
+
+            if (pendingDivider != null) //Offset divider add in order to not render a divider below the last filter option
+            {
+                Dividers.Add(pendingDivider);
+                Container.AddChild(pendingDivider);
+            }
+
+            pendingDivider = divider;
+            subObjects.Add(option);
+        }
+
+        public void InsertAt(FilterCheckBox option, int index)
+        {
+            if (index < 0)
+                throw new IndexOutOfRangeException();
+
+            if (index >= Options.Count)
+            {
+                AddOption(option);
+                return;
+            }
+
+            Options.Insert(index, option);
+            Filters.Insert(index, option.label);
+
+            filtersDirty = true;
+        }
+
+        private FSprite createDividerSprite()
+        {
+            return new FSprite("pixel", true)
+            {
+                x = 684f - pos.x,
+                y = 571f - 37f * (Dividers.Count + 1),
+                scaleX = 270f,
+                color = new Color(0.4f, 0.4f, 0.4f)
+            };
+        }
+
+        public bool HasCheckedOptions(FilterCheckBox excludeThis)
+        {
+            return Options.Count == 1 || Options.Exists(o => o != excludeThis && !o.FilterImmune && o.Checked);
+        }
+
+        public bool GetChecked(CheckBox box)
+        {
+            return box.Checked;
+        }
+
+        public void SetChecked(CheckBox box, bool checkState)
+        {
+            if (box.buttonBehav.greyedOut) return;
+
+            //TODO: Comments
+            if (!DoubleClick && LastFilterChanged == box && DoubleClickProtection > 0)
+            {
+                DoubleClick = true;
+                DoubleClickProtection = 100;
+
+                menu.PlaySound(SoundID.MENU_Player_Join_Game);
+                //SetChecked(box, true);
+            }
+
+            box.Checked = checkState;
+            LastFilterChanged = box;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (filtersDirty)
+            {
+                RefreshFilters();
+                filtersDirty = false;
+            }
+
+            if (DoubleClickProtection > 0)
+            {
+                DoubleClickProtection--;
+                return;
+            }
+            DoubleClick = false;
+        }
+
+        /// <summary>
+        /// Changes all associated sprite positions according to index sorting
+        /// </summary>
+        public void RefreshFilters()
+        {
+            LastFilterChanged = null;
+
+            for (int i = 0; i < Options.Count; i++)
+            {
+                FilterCheckBox filterOption = Options[i];
+                MenuLabel filterLabel = filterOption.label;
+
+                filterOption.pos.y = filterOption.lastPos.y = 577f - 37f * i;
+                filterLabel.pos.y = filterLabel.lastPos.y = 590f - 37f * i;
+            }
+
+            //Check if dividers need to be updated
+            int dividersNeeded = Options.Count - Dividers.Count - 1;
+
+            if (dividersNeeded > 0)
+            {
+                pendingDivider = null;
+
+                //Reset Y positions for all dividers
+                for(int i = 0; i< Dividers.Count;  i++)
+                    Dividers[i].y = 571f - 37f * (i + 1); //TODO: Check padding is correct
+
+                //Add any extra dividers as necessary
+                while (dividersNeeded > 0)
+                {
+                    FSprite divider = createDividerSprite();
+
+                    Dividers.Add(divider);
+                    Container.AddChild(divider);
+                    dividersNeeded--;
+                }
+
+                pendingDivider = createDividerSprite(); //It is expected to there to be an FSprite stored here
+            }
+        }
+    }
+
+    public class FilterCheckBox: CheckBox
+    {
+        /// <summary>
+        /// This checkbox should be treated more like a secondary filter, and will be ignored by the primary filter criteria
+        /// </summary>
+        public bool FilterImmune;
+
+        public FilterCheckBox(Menu.Menu menu, FilterOptions owner, IOwnCheckBox reportTo, Vector2 pos, float textWidth, string displayText, string IDString, bool textOnRight = false) : base(menu, owner, reportTo, pos, textWidth, displayText, IDString, textOnRight)
+        {
+            Checked = true;
+        }
+
+        public FilterCheckBox(Menu.Menu menu, FilterOptions owner, IOwnCheckBox reportTo, Vector2 pos, MenuLabel label, string IDString) : this(menu, owner, reportTo, pos, 0, string.Empty, IDString)
+        {
+            subObjects.Remove(this.label);
+            subObjects.Add(this.label = label);
+        }
+
+        public override void Clicked()
+        {
+            //Check if this option is enabled, and is allowed to be checked on/off
+            if (buttonBehav.greyedOut || (!FilterImmune && !(owner as FilterOptions).HasCheckedOptions(this))) return;
+
+            base.Clicked();
+        }
+    }
+}
---
+++ RandomStartsFix/Interface/ScrollablePage.cs
@@ -0,0 +1,81 @@
+﻿using Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ExpeditionRegionSupport.Interface
+{
+    public class ScrollablePage : Page
+    {
+        private float baseAlpha;
+
+        /// <summary>
+        /// Returns the alpha value used for updating scroll position multiplied by a factor value.
+        /// This reflects the active alpha for children sprites. 
+        /// </summary>
+        public float Alpha => baseAlpha * 0.95f;
+        public float BaseAlpha => baseAlpha;
+        public float LastAlpha;
+        public float CurrentAlpha;
+        public float TargetAlpha = 1f;
+        public float ScrollOffset = 100f;
+
+        /// <summary>
+        /// The process of moving the page from its default position to its viewable position
+        /// </summary>
+        public bool Opening;
+
+        /// <summary>
+        /// The process of moving the page from its viewable position to its default position
+        /// </summary>
+        public bool Closing;
+
+        /// <summary>
+        /// A flag that indicates that the page has successfully closed.
+        /// </summary>
+        public bool HasClosed;
+
+        public ScrollablePage(Menu.Menu menu, MenuObject owner, string name, int index) : base(menu, owner, name, index)
+        {
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            LastAlpha = CurrentAlpha;
+            CurrentAlpha = Mathf.Lerp(CurrentAlpha, TargetAlpha, 0.2f);
+
+            if (Opening)
+            {
+                HasClosed = false;
+
+                if (pos.y <= 0.01f)
+                    Opening = false;
+            }
+
+            if (Closing && Math.Abs(CurrentAlpha - TargetAlpha) < 0.09f)
+            {
+                HasClosed = true;
+                Closing = false;
+            }
+        }
+
+        public override void GrafUpdate(float timeStacker)
+        {
+            base.GrafUpdate(timeStacker); //subObjects GrafUpdate gets handled in base
+
+            if (Opening || Closing)
+            {
+                baseAlpha = Mathf.Pow(Mathf.Max(0f, Mathf.Lerp(LastAlpha, CurrentAlpha, timeStacker)), 1.5f);
+
+                foreach (MenuObject child in subObjects)
+                    child.SetAlpha(Alpha);
+            }
+
+            pos.y = Mathf.Lerp(menu.manager.rainWorld.options.ScreenSize.y + 100f, 0.01f, (baseAlpha < 0.999f) ? baseAlpha : 1f);
+        }
+    }
+}
