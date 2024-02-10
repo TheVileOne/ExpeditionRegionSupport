7c04f35ef88f1ad099f01d7f14a13483c1720438
Fluffball <xeolite@yahoo.com>
2024-02-09 21:56:02 -0500

    Remove unused code

--- RandomStartsFix/Interface/ExpeditionSettingsDialog.cs
+++ RandomStartsFix/Interface/ExpeditionSettingsDialog.cs
@@ -20,7 +20,7 @@ namespace ExpeditionRegionSupport.Interface
             set => CWT.Page = value;
         }
 
-        #region InitStrings
+        #region InitValues
 
         /// <summary>
         /// The common distance between two vertically adjacent selectables 
@@ -34,8 +34,6 @@ namespace ExpeditionRegionSupport.Interface
 
         public readonly string HeaderText = "SETTINGS";
         public readonly string SubHeaderText = "Region filter";
-
-        private string closeButtonText = "CLOSE";
 
         #endregion
 
@@ -65,65 +63,6 @@ namespace ExpeditionRegionSupport.Interface
             ModMerger modMerger = new ModMerger();
 
             //TODO
-        }       
-
-        private void initializeDialog()
-        {
-            initializeBase();
-
-            float[] screenOffsets = RWCustom.Custom.GetScreenOffsets();
-            leftAnchor = screenOffsets[0];
-            rightAnchor = screenOffsets[1];
-
-            //Move to scroll start positions
-            MainPage.pos.y = MainPage.pos.y + 2000f;
-
-            initializeHeaders();
-            initializeCancelButton();
-
-            InitializeCheckBoxes();
-
-            opening = true;
-            targetAlpha = 1f;
-        }
-
-        private void initializeHeaders()
-        {
-            pageTitle = new FSprite("filters", true);
-            pageTitle.SetAnchor(0.5f, 0f);
-            pageTitle.SetPosition(720f, 680f);
-
-            MainPage.Container.AddChild(pageTitle);
-
-            if (manager.rainWorld.options.language != InGameTranslator.LanguageID.English)
-            {
-                localizedSubtitle = new MenuLabel(this, MainPage, Translate("-SETTINGS-"), new Vector2(683f, 740f), default, false, null);
-                localizedSubtitle.label.color = new Color(0.5f, 0.5f, 0.5f);
-                MainPage.subObjects.Add(localizedSubtitle);
-            }
-
-            heading = new MenuLabel(this, MainPage, Translate("REGION FILTER"), new Vector2(683f, 655f), default, false, null);
-            heading.label.color = new Color(0.7f, 0.7f, 0.7f);
-            MainPage.subObjects.Add(heading);
-            description = new MenuLabel(this, MainPage, Translate("Toggle which regions are spawnable"), new Vector2(683f, 635f), default, false, null);
-            description.label.color = new Color(0.7f, 0.7f, 0.7f);
-            MainPage.subObjects.Add(description);
-        }
-
-        private void initializeCancelButton()
-        {
-            MainPage.AddSubObject(CreateButton("CLOSE", "CLOSE", new Vector2(683f, 120f)));
-            return;
-            string closeButtonTranslation = Translate(closeButtonText);
-
-            //Adjust button width to accomodate varying translation lengths
-            float num = Math.Max(85f, Menu.Remix.MixedUI.LabelTest.GetWidth(closeButtonTranslation, false) + 10f);
-
-            //cancelButton = new SimpleButton(this, MainPage, closeButtonTranslation, closeButtonSignal, new Vector2(683f - num / 2f, 120f), new Vector2(num, 35f));
-            cancelButton.nextSelectable[0] = cancelButton;
-            cancelButton.nextSelectable[2] = cancelButton;
-
-            MainPage.subObjects.Add(cancelButton);
         }
 
         public void InitializeButtons()
@@ -217,15 +156,15 @@ namespace ExpeditionRegionSupport.Interface
             }
             else if (checkBoxIndex > 0)
             {
-               /*
-                FSprite dividerSprite = new FSprite("pixel", true);
+                /*
+                 FSprite dividerSprite = new FSprite("pixel", true);
 
-                dividerSprite.SetPosition(actualDividerPosition);
-                dividerSprite.scaleX = 270f;
-                dividerSprite.color = new Color(0.4f, 0.4f, 0.4f);
-                container.AddChild(dividerSprite);
-                dividers.Add(dividerSprite);
-               */
+                 dividerSprite.SetPosition(actualDividerPosition);
+                 dividerSprite.scaleX = 270f;
+                 dividerSprite.color = new Color(0.4f, 0.4f, 0.4f);
+                 container.AddChild(dividerSprite);
+                 dividers.Add(dividerSprite);
+                */
             }
 
             return checkBox;
@@ -253,48 +192,6 @@ namespace ExpeditionRegionSupport.Interface
             }
 
             base.Singal(sender, signal);
-        }
-
-        private void initializeBase()
-        {
-            initializeMenuBase();
-            initializeDialogBase();
-        }
-
-        private void initializeMenuBase()
-        {
-            pages = new List<Page>();
-            container = new FContainer();
-            Futile.stage.AddChild(container);
-            mySoundLoopName = string.Empty;
-            cursorContainer = new FContainer();
-            currentPage = 0;
-
-            infoLabel = new FLabel(RWCustom.Custom.GetFont(), string.Empty);
-            infoLabel.y = Mathf.Max(0.01f + manager.rainWorld.options.SafeScreenOffset.y, 20.01f);
-            infoLabel.x = manager.rainWorld.screenSize.x / 2f + 0.01f;
-            Futile.stage.AddChild(infoLabel);
-
-            UpdateInfoText();
-        }
-
-        private void initializeDialogBase()
-        {
-            pages.Add(MainPage);
-            pos = new Vector2((manager.rainWorld.options.ScreenSize.x - size.x) * 0.5f, (manager.rainWorld.options.ScreenSize.x - size.y) * 0.5f);
-            darkSprite = new FSprite("pixel", true)
-            {
-                color = new Color(0f, 0f, 0f),
-                anchorX = 0f,
-                anchorY = 0f,
-                scaleX = manager.rainWorld.screenSize.x + 2f,
-                scaleY = manager.rainWorld.screenSize.x + 2f,
-                x = -1f,
-                y = -1f,
-                alpha = 0f
-            };
-
-            container.AddChild(darkSprite);
         }
     }
 }
