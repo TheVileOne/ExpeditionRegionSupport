3a719179419c0afc15359904eb938d13b3be79c5
Fluffball <xeolite@yahoo.com>
2024-02-15 21:32:09 -0500

    Replace filter hook with one with better mod support

--- ExpeditionRegionSupport/Challenges/ChallengeFilter.cs
+++ ExpeditionRegionSupport/Challenges/ChallengeFilter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Expedition;
+using Extensions;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using static ExpeditionRegionSupport.ListUtils;
@@ -13,49 +15,74 @@ namespace ExpeditionRegionSupport.Challenges
     {
         public static ChallengeFilterOptions CurrentFilter;
 
+        /// <summary>
+        /// The Expedition challenge that the filter is handling, or is about to handle
+        /// </summary>
+        public static Challenge FilterTarget;
+
+        public static bool HasFilter => CurrentFilter != ChallengeFilterOptions.None;
+
         public static void ApplyHooks()
         {
             On.Expedition.EchoChallenge.Generate += EchoChallenge_Generate;
             IL.Expedition.EchoChallenge.Generate += EchoChallenge_Generate;
         }
 
-        private static Expedition.Challenge EchoChallenge_Generate(On.Expedition.EchoChallenge.orig_Generate orig, Expedition.EchoChallenge self)
+        private static Challenge EchoChallenge_Generate(On.Expedition.EchoChallenge.orig_Generate orig, EchoChallenge self)
         {
-            Plugin.Logger.LogInfo("Generating Echo Challenge");
-            return orig(self);
+            FilterTarget = self;
+
+            try
+            {
+                return orig(self);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                //This will trigger if the list is empty, and some mod didn't check the list count after applying a mod-specific filter
+                Plugin.Logger.LogWarning("Filter encountered an IndexOutOfRangeException");
+                return null; //Return null to indicate that no challenges of the current type can be chosen
+            }
+            finally
+            {
+                FilterTarget = null;
+            }
         }
 
         private static void EchoChallenge_Generate(ILContext il)
         {
             ILCursor cursor = new ILCursor(il);
 
-            cursor.GotoNext(MoveType.Before, x => x.MatchStloc(0)); //Move to list assignment
-            cursor.Emit(OpCodes.Pop);
-            cursor.EmitDelegate(createConstrainedEchoList); //Replace list with a ConstrainedList
+            MethodInfo getCountMethod = typeof(List<string>).GetMethod("get_Count");
 
-            MethodInfo listAdd = typeof(List<string>).GetMethod("Add");
-            MethodInfo newListAdd = typeof(ConstrainedList<string>).GetMethod("Add");
+            //Apply filter logic
 
-            //Replace Add call with Add call by a custom List
-            cursor.GotoNext(MoveType.Before, x => x.MatchCallOrCallvirt(listAdd));
-            cursor.Remove();
+            cursor.GotoNext(MoveType.After, //Move us closer to loop process code
+                x => x.MatchAdd(),
+                x => x.MatchStloc(2),
+                x => x.MatchLdloc(2));
+            cursor.GotoNext(MoveType.After, //Move to after loop finishes
+                x => x.MatchCallOrCallvirt(getCountMethod),
+                x => x.Match(OpCodes.Blt));
+            cursor.Emit(OpCodes.Ldloc_0); //Push list of echo region options on the stack
+            cursor.EmitDelegate(applyEchoChallengeFilter);
 
-            cursor.Emit(OpCodes.Call, newListAdd);
+            //Handle list post filter. An empty list will throw an exception
+
+            cursor.Emit(OpCodes.Ldloc_0); //Push list back on the stack to check its count
+            cursor.Emit(OpCodes.Call, getCountMethod);
+            cursor.Emit(OpCodes.Ldc_I4_0);
+            cursor.BranchStart(OpCodes.Bgt); //Branch to main logic, or any additional mod-specific filters when list isn't empty
+            cursor.Emit(OpCodes.Ldnull); //Return null to indicate that no challenges of the current type can be chosen
+            cursor.Emit(OpCodes.Ret);
+            cursor.BranchFinish();
         }
 
-        private static ConstrainedList<string> createConstrainedEchoList()
+        private static void applyEchoChallengeFilter(List<string> echoRegions)
         {
-            return new ConstrainedList<string>(regionCode =>
-            {
-                switch (CurrentFilter)
-                {
-                    case ChallengeFilterOptions.None:
-                        return true;
-                    case ChallengeFilterOptions.VisitedRegions:
-                        return Plugin.RegionsVisited.Contains(regionCode);
-                }
-                return true;
-            });
+            if (!HasFilter) return;
+
+            if (CurrentFilter == ChallengeFilterOptions.VisitedRegions)
+                echoRegions.RemoveAll(r => !Plugin.RegionsVisited.Contains(r));
         }
     }
 
