56d207670138538cbfeab9bde948d97731303eb5
Fluffball <xeolite@yahoo.com>
2024-02-18 15:36:11 -0500

    Implement ChallengeFilter

---
+++ ExpeditionRegionSupport/Filters/ChallengeFilter.cs
@@ -0,0 +1,28 @@
+﻿using ExpeditionRegionSupport.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionRegionSupport.Filters
+{
+    public class ChallengeFilter : ListFilter<string>
+    {
+        public ChallengeFilterOptions FilterID;
+
+        public override bool Enabled => Challenges.ChallengeFilter.CurrentFilter == FilterID;
+
+        public ChallengeFilter(ChallengeFilterOptions filterID) : base(PrepareFilter(filterID))
+        {
+        }
+
+        protected static List<string> PrepareFilter(ChallengeFilterOptions filterID)
+        {
+            if (filterID == ChallengeFilterOptions.VisitedRegions)
+                return Plugin.RegionsVisited;
+
+            return new List<string>();
+        }
+    }
+}
