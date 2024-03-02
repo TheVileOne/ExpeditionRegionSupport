bec546a0873321e29b82892d5e0448018da6193a
Fluffball <xeolite@yahoo.com>
2024-03-02 00:59:13 -0500

    Include CWT

--- ExpeditionRegionSupport/Filters/Utils/ChallengeUtils.cs
+++ ExpeditionRegionSupport/Filters/Utils/ChallengeUtils.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,18 @@ namespace ExpeditionRegionSupport.Filters.Utils
 {
     public static class ChallengeUtils
     {
+        public class ChallengeCWT
+        {
+            /// <summary>
+            /// A flag that indicates that this challenge cannot be chosen for an active mission
+            /// </summary>
+            public bool Disabled = false;
+        }
+
+        public static readonly ConditionalWeakTable<Challenge, ChallengeCWT> challengeCWT = new();
+
+        public static ChallengeCWT GetCWT(this Challenge self) => challengeCWT.GetValue(self, _ => new());
+
         /// <summary>
         /// Gets the challenge reference used by ChallengeOrganizer to manage challenge assignment
         /// </summary>
