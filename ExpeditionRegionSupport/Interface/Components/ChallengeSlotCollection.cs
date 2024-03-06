3668375a411fa173d6914d5b4aef2c5f7a974398
Fluffball <xeolite@yahoo.com>
2024-03-05 22:25:01 -0500

    Include class template for handling a challenge slot collection

---
+++ ExpeditionRegionSupport/Interface/Components/ChallengeSlotCollection.cs
@@ -0,0 +1,61 @@
+﻿using Expedition;
+using ExpeditionRegionSupport.Filters.Utils;
+using Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionRegionSupport.Interface.Components
+{
+    public class ChallengeSlotCollection
+    {
+        /// <summary>
+        /// The number of slot buttons per challenge page
+        /// </summary>
+        public const int SLOTS_PER_PAGE = 6;
+
+        public List<Challenge> Challenges;
+        public BigSimpleButton[] SlotButtons;
+
+        public ChallengeSelectPage Owner;
+
+        public ChallengeSlotCollection(ChallengeSelectPage collectionOwner, SlugcatStats.Name challengeOwner)
+        {
+            Owner = collectionOwner;
+            UpdateChallenges(challengeOwner);
+        }
+
+        public void AddSlot()
+        {
+        }
+
+        public void RemoveSlot()
+        {
+        }
+
+        public void GenerateSlots(int slotsWanted)
+        {
+        }
+
+        public void RerollChallenges()
+        {
+        }
+
+        public void ToggleHidden(int slotID)
+        {
+        }
+
+        public void UpdateChallenges(SlugcatStats.Name challengeOwner)
+        {
+            Challenges = ChallengeUtils.GetChallenges(challengeOwner);
+            UpdateChallengeButtons();
+        }
+
+        public void UpdateChallengeButtons()
+        {
+            Owner.UpdateChallengeButtons();
+        }
+    }
+}
