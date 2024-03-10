05918dbd97b17261b2434927bdd1d797126d1d6e
Fluffball <xeolite@yahoo.com>
2024-03-10 19:11:02 -0400

    WIP exception handling logic

---
+++ ExpeditionRegionSupport/ExceptionHandling/ExceptionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionRegionSupport.ExceptionHandling
+{
+    public class ExceptionHandler
+    {
+        public int FailCode = -1;
+        public Func<bool> CheckCondition;
+
+        public void HandleException<T>(int failCode, T triggerObject, Predicate<T> exceptionTrigger)
+        {
+            if (exceptionTrigger(triggerObject))
+                FailCode = failCode;
+        }
+
+        public void HandleNullException(int failCode, object triggerObject)
+        {
+            if (CheckNull(triggerObject))
+                FailCode = failCode;
+        }
+
+        public bool CheckNull(object obj)
+        {
+            return obj == null;
+        }
+    }
+}
