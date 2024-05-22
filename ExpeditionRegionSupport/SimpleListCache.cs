46d2d80d1d7728165a7fd13bb55735ac3b84e119
Fluffball <xeolite@yahoo.com>
2024-05-21 22:01:16 -0400

    Implement base class for caching data

---
+++ ExpeditionRegionSupport/SimpleListCache.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionRegionSupport
+{
+    public class SimpleListCache<T>
+    {
+        /// <summary>
+        /// A unique identifier that can be used to distinguish between two cache instances
+        /// </summary>
+        protected int CacheID = -1;
+
+        protected List<T> _items = new List<T>();
+
+        /// <summary>
+        /// A collection of stored items
+        /// </summary>
+        public List<T> Items
+        {
+            get => _items;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+
+                //This reference should not be overwritten. Transfer values to existing reference instead.
+                _items.Clear();
+                Store(value);
+            }
+        }
+
+        /// <summary>
+        /// Stores an item in the item cache
+        /// </summary>
+        public virtual T Store(T item)
+        {
+            _items.Add(item);
+            return item;
+        }
+
+        /// <summary>
+        /// Stores item in the item cache from a provided IEnumerable
+        /// </summary>
+        public virtual void Store(IEnumerable<T> items)
+        {
+            _items.AddRange(items);
+        }
+    }
+}
