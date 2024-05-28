e007021380c86e10307dbc586267b92147e327a6
Fluffball <xeolite@yahoo.com>
2024-05-27 22:19:10 -0400

    Implement an IEnumerable with autocache functionality

---
+++ ExpeditionRegionSupport/Data/CachedEnumerable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionRegionSupport.Data
+{
+    public class CachedEnumerable<T> : IEnumerable<T>
+    {
+        protected IEnumerable<T> InnerEnumerable;
+
+        /// <summary>
+        /// Enumerated values are stored here when values are returned from InnerEnumerable
+        /// </summary>
+        public readonly IList<T> EnumeratedValues = new List<T>();
+
+        public CachedEnumerable(IEnumerable<T> innerEnumerable)
+        {
+            InnerEnumerable = innerEnumerable;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (InnerEnumerable != null)
+            {
+                foreach (var item in InnerEnumerable)
+                {
+                    EnumeratedValues.Add(item);
+                    yield return item;
+                }
+                InnerEnumerable = null;
+            }
+            else
+            {
+                foreach (var item in EnumeratedValues)
+                    yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
