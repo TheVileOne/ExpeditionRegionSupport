997254e7f8c148438e846d63389ec0c526e62293
Fluffball <xeolite@yahoo.com>
2024-06-01 16:47:32 -0400

    Compare, and equality implementation

--- ExpeditionRegionSupport/Data/Range.cs
+++ ExpeditionRegionSupport/Data/Range.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ExpeditionRegionSupport.Data
 {
-    public struct Range
+    public struct Range : IEquatable<Range>, IComparable<Range>
     {
         public int Start;
         public int End;
@@ -26,6 +26,54 @@ namespace ExpeditionRegionSupport.Data
         {
             return new Range(Math.Min(rangeA.Start, rangeB.Start), Math.Max(rangeA.End, rangeB.End));
         }
+
+        public bool Equals(Range other)
+        {
+            return Start == other.Start && End == other.End;
+        }
+
+        public int CompareTo(Range other) => CompareByEarliestStart(other);
+
+        public int CompareByEarliestStart(Range other)
+        {
+            if (Equals(other))
+                return 0;
+
+            //Start of range is either less than other, or start in the same place, and ends earlier than other
+            if (Start < other.Start || (Start == other.Start && End < other.End))
+                return -1;
+
+            //other is considered greater in all other circumstances
+            return 1;
+        }
+
+        public int CompareByLength(Range other)
+        {
+            int rangeDiff = ValueRange - other.ValueRange;
+
+            if (rangeDiff != 0) //Output will be negative when the value range of this is less than the value range of other
+                return rangeDiff;
+            return CompareByEarliestStart(other);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Start + "," + End;
+        }
+
+        public static bool operator ==(Range left, Range right) => left.Equals(right);
+
+        public static bool operator !=(Range left, Range right) => !left.Equals(right);
 
         public static Range Zero => _zero;
         public static Range NegativeOne => _negativeOne;
