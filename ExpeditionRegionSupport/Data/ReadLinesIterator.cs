4e9adb9d76b4f5b6371eafe2dd9f8bb3343eabf1
Fluffball <xeolite@yahoo.com>
2024-06-02 20:56:00 -0400

    Make IDisposable

--- ExpeditionRegionSupport/Data/ReadLinesIterator.cs
+++ ExpeditionRegionSupport/Data/ReadLinesIterator.cs
@@ -7,14 +7,16 @@ using System.Threading.Tasks;
 
 namespace ExpeditionRegionSupport.Data
 {
-    public class ReadLinesIterator
+    public class ReadLinesIterator : IDisposable
     {
-        protected StreamReader Stream { get; }
+        protected StreamReader Stream { get; set; }
 
         /// <summary>
         /// Apply conditions that pass over text data that matches certain criteria
         /// </summary>
         public List<StringDelegates.Validate> SkipConditions = new List<StringDelegates.Validate>();
+
+        private bool isDisposed;
 
         public ReadLinesIterator(StreamReader stream)
         {
@@ -48,6 +50,29 @@ namespace ExpeditionRegionSupport.Data
         private bool checkSkipConditions(string line)
         {
             return SkipConditions.Exists(hasFailedCheck => hasFailedCheck(line));
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                    Stream = null;
+                isDisposed = true;
+            }
+        }
+
+        ~ReadLinesIterator()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
