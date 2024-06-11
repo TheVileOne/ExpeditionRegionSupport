6b6c4ca2707bf7e148707ee00531c5fc8b114bc0
Fluffball <xeolite@yahoo.com>
2024-06-10 20:31:04 -0400

    Implement data transfer class

---
+++ ExpeditionRegionSupport/Data/Logging/DataTransferController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionRegionSupport.Data.Logging
+{
+    /// <summary>
+    /// A static class used to assist in the transfer of data strings from one mod to another
+    /// </summary>
+    public static class DataTransferController
+    {
+        public static ExtEnumType DataHandler;
+
+        public static List<string> UnhandledMessages => DataHandler.entries;
+
+        /// <summary>
+        /// Handle activities that are required for proper functionality of the class
+        /// </summary>
+        public static void Initialize()
+        {
+            foreach (KeyValuePair<Type, ExtEnumType> entry in ExtEnumBase.valueDictionary)
+            {
+                if (entry.Key.Name.Contains(nameof(DataMessage)))
+                {
+                    DataHandler = entry.Value;
+                    break;
+                }
+            }
+
+            if (DataHandler == null)
+            {
+                DataMessage dataMessage = new DataMessage("data", false);
+                DataHandler = ExtEnumBase.valueDictionary[dataMessage.GetType()];
+            }
+        }
+
+        public static void SendMessage(string message)
+        {
+            DataHandler.AddEntry(message);
+        }
+    }
+
+    /// <summary>
+    /// A class used to store data strings for other mods to recognize and process
+    /// </summary>
+    public class DataMessage : ExtEnum<string>
+    {
+        public DataMessage(string value, bool register = false) : base(value, register)
+        {
+        }
+    }
+}
