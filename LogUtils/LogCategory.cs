3e46cf88b896628928c38f5f5d2739659faacc72
Fluffball <xeolite@yahoo.com>
2024-06-30 18:06:33 -0400

    Include ExtEnum for log categories

---
+++ LogUtils/LogCategory.cs
@@ -0,0 +1,19 @@
+﻿namespace LogUtils
+{
+    public class LogCategory : ExtEnum<LogCategory>
+    {
+        public LogCategory(string value, bool register = false) : base(value, register)
+        {
+        }
+
+        public static readonly LogCategory All = new LogCategory("All", true);
+        public static readonly LogCategory None = new LogCategory("None", true);
+        public static readonly LogCategory Debug = new LogCategory("Debug", true);
+        public static readonly LogCategory Info = new LogCategory("Info", true);
+        public static readonly LogCategory Message = new LogCategory("Message", true);
+        public static readonly LogCategory Important = new LogCategory("Important", true);
+        public static readonly LogCategory Warning = new LogCategory("Warning", true);
+        public static readonly LogCategory Error = new LogCategory("Error", true);
+        public static readonly LogCategory Fatal = new LogCategory("Fatal", true);
+    }
+}
