6b1c2d27ea607bbde48b5c669725e98a9f739cbb
Fluffball <xeolite@yahoo.com>
2024-08-27 19:11:01 -0400

    Unused code

---
+++ LogUtils/RequestValidation.cs
@@ -0,0 +1,49 @@
+﻿using LogUtils.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUtils
+{
+    public static class RequestValidation
+    {
+        public static RejectionReason CheckAccessPeriod(LogID logFile)
+        {
+            //Check this first - it is useful to reject as ShowLogsNotInitialized before rejecting as LogUnavailable
+            if (logFile.Properties.ShowLogsAware && !RainWorld.ShowLogs)
+            {
+                if (RWInfo.LatestSetupPeriodReached < RWInfo.SHOW_LOGS_ACTIVE_PERIOD)
+                    return RejectionReason.ShowLogsNotInitialized;
+                else
+                    return RejectionReason.LogDisabled;
+            }
+
+            //Makes sure that the log file can be initialized
+            if (!logFile.Properties.LogSessionActive && RWInfo.LatestSetupPeriodReached < logFile.Properties.AccessPeriod)
+                return RejectionReason.LogUnavailable;
+
+            return RejectionReason.None;
+        }
+
+        public static void ValidateAccessPeriod(LogRequest request)
+        {
+            LogID logFile = request.Data.ID;
+
+            //Check this first - it is useful to reject as ShowLogsNotInitialized before rejecting as LogUnavailable
+            if (logFile.Properties.ShowLogsAware && !RainWorld.ShowLogs)
+            {
+                if (RWInfo.LatestSetupPeriodReached < RWInfo.SHOW_LOGS_ACTIVE_PERIOD)
+                    request.Reject(RejectionReason.ShowLogsNotInitialized);
+                else
+                    request.Reject(RejectionReason.LogDisabled);
+                return;
+            }
+
+            //Makes sure that the log file can be initialized
+            if (!logFile.Properties.LogSessionActive && RWInfo.LatestSetupPeriodReached < logFile.Properties.AccessPeriod)
+                request.Reject(RejectionReason.LogUnavailable);
+        }
+    }
+}
