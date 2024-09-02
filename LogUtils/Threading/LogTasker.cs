86279b2df4cf90e8a5aa9c059653128afbfe07fa
Fluffball <xeolite@yahoo.com>
2024-09-02 17:58:54 -0400

    Unused code

---
+++ LogUtils/Threading/LogTasker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogUtils.Threading
+{
+    public static class LogTasker
+    {
+        public static Task ActiveTask;
+
+        public static Queue<Task> AwaitingTasks = new Queue<Task>();
+
+        public static Thread TaskThread;
+
+        /// <summary>
+        /// The ThreadID currently processing logging tasks, -1 if no tasks are running
+        /// </summary>
+        public static int TaskThreadID => TaskThread != null ? TaskThread.ManagedThreadId : -1;
+
+        public static bool RunningTasksOnCurrentThread => Thread.CurrentThread.ManagedThreadId == TaskThreadID;
+
+        public static void RunTask(Action work)
+        {
+            Task task = new Task(work);
+
+            if (ActiveTask != null && !ActiveTask.IsCompleted)
+            {
+                AwaitingTasks.Enqueue(task);
+            }
+            else
+            {
+                ActiveTask = task;
+
+                ActiveTask.Start();
+                ActiveTask.ContinueWith(taskAfter);
+
+                static void taskAfter(Task t)
+                {
+                    t.Dispose();
+
+                    if (AwaitingTasks.Any())
+                    {
+                        ActiveTask = AwaitingTasks.Dequeue();
+                        ActiveTask.Start();
+                        ActiveTask.ContinueWith(taskAfter);
+                    }
+                    else
+                    {
+                        ActiveTask = null;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LogUtils/UtilityLogger.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUtils
+{
+    public static class UtilityLogger
+    {
+        /*
+        private static ManualLogSource logger;
+
+        internal static void Initialize()
+        {
+            logger = BepInEx.Logging.Logger.Sources.FirstOrDefault(l => l.SourceName == "LogUtils") as ManualLogSource
+                  ?? BepInEx.Logging.Logger.CreateLogSource("LogUtils");
+        }
+
+        public void LogFatal(object data)
+        {
+            Log(LogLevel.Fatal, data);
+        }
+
+        public void LogError(object data)
+        {
+            Log(LogLevel.Error, data);
+        }
+
+        public void LogWarning(object data)
+        {
+            Log(LogLevel.Warning, data);
+        }
+
+        public void LogMessage(object data)
+        {
+            Log(LogLevel.Message, data);
+        }
+
+        public void LogInfo(object data)
+        {
+            Log(LogLevel.Info, data);
+        }
+
+        public void LogDebug(object data)
+        {
+            Log(LogLevel.Debug, data);
+        }
+        */
+    }
+}
