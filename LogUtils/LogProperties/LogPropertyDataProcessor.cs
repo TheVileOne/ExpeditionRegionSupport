66a9758f8bbc909c801923224d0274a188fbc91b
Fluffball <xeolite@yahoo.com>
2024-10-20 01:52:44 -0400

    Use collection initializer

--- LogUtils/LogProperties/LogPropertyDataProcessor.cs
+++ LogUtils/LogProperties/LogPropertyDataProcessor.cs
@@ -50,23 +50,24 @@ namespace LogUtils.Properties
             LogProperties properties = new LogProperties(idString, filename, path);
 
             //Property setters are inaccesible. Define delegate wrappers for each one, and store in a dictionary
-            OrderedDictionary fieldAssignments = new OrderedDictionary();
+            OrderedDictionary fieldAssignments = new OrderedDictionary
+            {
+                [DataFields.VERSION]              = new Action(() => properties.Version            = dataFields[DataFields.VERSION]),
+                [DataFields.ALTFILENAME]          = new Action(() => properties.AltFilename        = dataFields[DataFields.ALTFILENAME]),
+                [DataFields.ORIGINAL_PATH]        = new Action(() => properties.OriginalFolderPath = dataFields[DataFields.ORIGINAL_PATH]),
+                [DataFields.LAST_KNOWN_PATH]      = new Action(() => properties.LastKnownFilePath  = dataFields[DataFields.LAST_KNOWN_PATH]),
+                [DataFields.TAGS]                 = new Action(() => properties.Tags               = dataFields[DataFields.TAGS].Split(',')),
+                [DataFields.LOGS_FOLDER_AWARE]    = new Action(() => properties.LogsFolderAware    = bool.Parse(dataFields[DataFields.LOGS_FOLDER_AWARE])),
+                [DataFields.LOGS_FOLDER_ELIGIBLE] = new Action(() => properties.LogsFolderEligible = bool.Parse(dataFields[DataFields.LOGS_FOLDER_ELIGIBLE])),
+                [DataFields.SHOW_LOGS_AWARE]      = new Action(() => properties.ShowLogsAware      = bool.Parse(dataFields[DataFields.SHOW_LOGS_AWARE])),
+                [DataFields.Intro.MESSAGE]        = new Action(() => properties.IntroMessage       = dataFields[DataFields.Intro.MESSAGE]),
+                [DataFields.Outro.MESSAGE]        = new Action(() => properties.OutroMessage       = dataFields[DataFields.Outro.MESSAGE]),
+                [DataFields.Intro.TIMESTAMP]      = new Action(() => properties.ShowIntroTimestamp = bool.Parse(dataFields[DataFields.Intro.TIMESTAMP])),
+                [DataFields.Outro.TIMESTAMP]      = new Action(() => properties.ShowOutroTimestamp = bool.Parse(dataFields[DataFields.Outro.TIMESTAMP])),
 
-            fieldAssignments[DataFields.VERSION]               = new Action(() => properties.Version             = dataFields[DataFields.VERSION]);
-            fieldAssignments[DataFields.ALTFILENAME]           = new Action(() => properties.AltFilename         = dataFields[DataFields.ALTFILENAME]);
-            fieldAssignments[DataFields.ORIGINAL_PATH]         = new Action(() => properties.OriginalFolderPath  = dataFields[DataFields.ORIGINAL_PATH]);
-            fieldAssignments[DataFields.LAST_KNOWN_PATH]       = new Action(() => properties.LastKnownFilePath   = dataFields[DataFields.LAST_KNOWN_PATH]);
-            fieldAssignments[DataFields.TAGS]                  = new Action(() => properties.Tags                = dataFields[DataFields.TAGS].Split(','));
-            fieldAssignments[DataFields.LOGS_FOLDER_AWARE]     = new Action(() => properties.LogsFolderAware     = bool.Parse(dataFields[DataFields.LOGS_FOLDER_AWARE]));
-            fieldAssignments[DataFields.LOGS_FOLDER_ELIGIBLE]  = new Action(() => properties.LogsFolderEligible  = bool.Parse(dataFields[DataFields.LOGS_FOLDER_ELIGIBLE]));
-            fieldAssignments[DataFields.SHOW_LOGS_AWARE]       = new Action(() => properties.ShowLogsAware       = bool.Parse(dataFields[DataFields.SHOW_LOGS_AWARE]));
-            fieldAssignments[DataFields.Intro.MESSAGE]         = new Action(() => properties.IntroMessage        = dataFields[DataFields.Intro.MESSAGE]);
-            fieldAssignments[DataFields.Outro.MESSAGE]         = new Action(() => properties.OutroMessage        = dataFields[DataFields.Outro.MESSAGE]);
-            fieldAssignments[DataFields.Intro.TIMESTAMP]       = new Action(() => properties.ShowIntroTimestamp  = bool.Parse(dataFields[DataFields.Intro.TIMESTAMP]));
-            fieldAssignments[DataFields.Outro.TIMESTAMP]       = new Action(() => properties.ShowOutroTimestamp  = bool.Parse(dataFields[DataFields.Outro.TIMESTAMP]));
-
-            fieldAssignments[DataFields.Rules.SHOW_CATEGORIES] = new Action(() => properties.ShowCategories.IsEnabled = bool.Parse(dataFields[DataFields.Rules.SHOW_CATEGORIES]));
-            fieldAssignments[DataFields.Rules.SHOW_LINE_COUNT] = new Action(() => properties.ShowLineCount.IsEnabled  = bool.Parse(dataFields[DataFields.Rules.SHOW_LINE_COUNT]));
+                [DataFields.Rules.SHOW_CATEGORIES] = new Action(() => properties.ShowCategories.IsEnabled = bool.Parse(dataFields[DataFields.Rules.SHOW_CATEGORIES])),
+                [DataFields.Rules.SHOW_LINE_COUNT] = new Action(() => properties.ShowLineCount.IsEnabled = bool.Parse(dataFields[DataFields.Rules.SHOW_LINE_COUNT]))
+            };
 
             IDictionaryEnumerator enumerator = fieldAssignments.GetEnumerator();
 
