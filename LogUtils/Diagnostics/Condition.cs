1a264d976b2888c4c099f5fb6db9cadfc9de45e7
Fluffball <xeolite@yahoo.com>
2024-12-13 19:15:12 -0500

    Begin work on condition testing framework

---
+++ LogUtils/Diagnostics/Condition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using static LogUtils.Diagnostics.Debug;
+
+namespace LogUtils.Diagnostics
+{
+    public static class Condition
+    {
+        public static BooleanAssert Check(bool condition) => new BooleanAssert(condition);
+        public static NumericAssert Check(double value) => new NumericAssert(value);
+        public static CollectionAssert<T> Check<T>(IEnumerable<T> enumerable) => new CollectionAssert<T>(enumerable);
+        public static ObjectAssert Check(object data) => new ObjectAssert(data);
+
+        public static BooleanAssert Assert(bool condition) => new BooleanAssert(condition);
+        public static NumericAssert Assert(double value) => new NumericAssert(value);
+        public static CollectionAssert<T> Assert<T>(IEnumerable<T> enumerable) => new CollectionAssert<T>(enumerable);
+        public static ObjectAssert Assert(object data) => new ObjectAssert(data);
+    }
+}
