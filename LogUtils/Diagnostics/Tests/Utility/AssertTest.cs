a3e2dde09186b9aa3b3ef7f69ca4860c5b24d0db
Fluffball <xeolite@yahoo.com>
2025-01-15 22:18:39 -0500

    Tests for assert logic now use deferred assert handling, with explicit notation for expected failures

--- LogUtils/Diagnostics/Tests/Utility/AssertTest.cs
+++ LogUtils/Diagnostics/Tests/Utility/AssertTest.cs
@@ -7,8 +7,23 @@ namespace LogUtils.Diagnostics.Tests.Utility
     {
         public string Name => "Test - Assert System";
 
+        private DeferredAssertHandler handler;
+
         public void Test()
         {
+            //Borrow the Logger instance from the current template
+            AssertHandler template = AssertHandler.CurrentTemplate as AssertHandler;
+
+            if (template == null)
+                template = AssertHandler.DefaultHandler;
+
+            handler = new DeferredAssertHandler(template.Logger)
+            {
+                Behavior = AssertBehavior.LogOnFail | AssertBehavior.LogOnPass,
+                FailResponse = "Fail",
+                PassResponse = "Pass"
+            };
+
             testCollectionDetection();
             testComparisonOfNullableCombinations();
             testEqualityOfNullableCombinations();
@@ -19,34 +34,56 @@ namespace LogUtils.Diagnostics.Tests.Utility
         /// </summary>
         private void testCollectionDetection()
         {
+            handler.Logger.Log("Testing: Collection asserts");
+
             //The implementation is sensitive to the type it stored as.
             ICollection<string> strings = new List<string> { "test", "data" };
             List<string> strings2 = new List<string> { "test", "data" };
             CustomCollection customCollection = new CustomCollection();
 
-            Assert.That(strings).IsNullOrEmpty();
-            Assert.That(strings2).HasItems();
-            Assert.That(customCollection.AsEnumerable()).IsNullOrEmpty();
+            handler.Logger.Log("Test 1");
+            Assert.That(strings, handler).IsNullOrEmpty();
+            handler.Logger.Log("Test 2");
+            Assert.That(strings2, handler).HasItems();
+            handler.Logger.Log("Test 3");
+            Assert.That(customCollection.AsEnumerable(), handler).IsNullOrEmpty();
+
+            //The first is expected to fail, but the next few are not
+            handler.HandleCurrent(expectation: Condition.State.Fail);
+            handler.HandleAll();
         }
 
         private void testEqualityOfNullableCombinations()
         {
+            handler.Logger.Log("Testing: Nullable equality asserts");
+
             UnityEngine.Vector2 testValue1 = UnityEngine.Vector2.one;
             UnityEngine.Vector2? testValue2 = null;
             UnityEngine.Vector2 testValue3 = UnityEngine.Vector2.zero;
 
-            Assert.That(testValue1).IsEqualTo(testValue3);    //Expect fail
-            Assert.That(testValue1).IsEqualTo(testValue2);    //Expect fail
-            Assert.That(testValue2).IsEqualTo(testValue2);
-            Assert.That(testValue1).IsEqualTo(testValue3);    //Expect fail
-            Assert.That(testValue1).DoesNotEqual(testValue3);
-            Assert.That(testValue1).DoesNotEqual(testValue2);
-            Assert.That(testValue2).DoesNotEqual(testValue2); //Expect fail
-            Assert.That(testValue2).DoesNotEqual(testValue3);
+            Assert.That(testValue1, handler).IsEqualTo(testValue3);    //Expect fail
+            Assert.That(testValue1, handler).IsEqualTo(testValue2);    //Expect fail
+            Assert.That(testValue2, handler).IsEqualTo(testValue2);
+            Assert.That(testValue1, handler).IsEqualTo(testValue3);    //Expect fail
+            Assert.That(testValue1, handler).DoesNotEqual(testValue3);
+            Assert.That(testValue1, handler).DoesNotEqual(testValue2);
+            Assert.That(testValue2, handler).DoesNotEqual(testValue2); //Expect fail
+            Assert.That(testValue2, handler).DoesNotEqual(testValue3);
+
+            handler.HandleCurrent(expectation: Condition.State.Fail)
+                   .HandleCurrent(expectation: Condition.State.Fail)
+                   .HandleCurrent()
+                   .HandleCurrent(expectation: Condition.State.Fail)
+                   .HandleCurrent()
+                   .HandleCurrent()
+                   .HandleCurrent(expectation: Condition.State.Fail)
+                   .HandleCurrent();
         }
 
         private void testComparisonOfNullableCombinations()
         {
+            handler.Logger.Log("Testing: Nullable comparison asserts");
+
             int testValue1 = 5,
                 testValue2 = 8;
 
@@ -57,25 +94,30 @@ namespace LogUtils.Diagnostics.Tests.Utility
              * Description: Non-nullable/Non-nullable
              * Expectation: Pass
              */
-            Assert.That(testValue1).IsLessThan(testValue2); //Is 5 less than 8?
+            Assert.That(testValue1, handler).IsLessThan(testValue2); //Is 5 less than 8?
 
             /*
              * Description: Non-nullable/Nullable
              * Expectation: Pass
              */
-            Assert.That(testValue1).IsLessThan(testNullable2); //Is 5 less than 6?
+            Assert.That(testValue1, handler).IsLessThan(testNullable2); //Is 5 less than 6?
 
             /*
              * Description: Nullable/Nullable
              * Expectation: Fail, when comparing two nullable structs, values can only be equal, or not equal
              */
-            Assert.That(testNullable2).IsLessThan(testNullable1); //Is 6 less than null?
+            Assert.That(testNullable2, handler).IsLessThan(testNullable1); //Is 6 less than null?
 
             /*
              * Description: Nullable/Non-nullable
              * Expectation: Fail, when comparing two nullable structs, values can only be equal, or not equal
              */
-            Assert.That(testNullable1).IsLessThan(testValue2); //Is null less than 8?
+            Assert.That(testNullable1, handler).IsLessThan(testValue2); //Is null less than 8?
+
+            handler.HandleCurrent()
+                   .HandleCurrent()
+                   .HandleCurrent(expectation: Condition.State.Fail)
+                   .HandleCurrent(expectation: Condition.State.Fail);
         }
 
         /// <summary>
