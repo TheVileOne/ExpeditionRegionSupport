91e4a4035e98d5a3f2458cde877e78b4b95b0476
Fluffball <xeolite@yahoo.com>
2025-03-04 19:01:23 -0500

    Safer ThreadSafeWorker implementation

--- LogUtils/Threading/ThreadSafeWorker.cs
+++ LogUtils/Threading/ThreadSafeWorker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace LogUtils.Threading
@@ -8,9 +9,16 @@ namespace LogUtils.Threading
     {
         private readonly IEnumerable<object> _locks;
 
+        /// <summary>
+        /// A flag that indicates whether locks should be extracted from the provided enumerable, and placed into an array before work starts.
+        /// <br>May be set to false if underlying enumerable is unlikely to be modified during work</br>
+        /// </summary>
+        public bool UseEnumerableWrapper = true;
+
         public ThreadSafeWorker(object lockObject)
         {
-            _locks = new[] { lockObject };
+            UseEnumerableWrapper = false;
+            _locks = [lockObject];
         }
 
         public ThreadSafeWorker(IEnumerable<object> locks)
@@ -20,20 +28,40 @@ namespace LogUtils.Threading
 
         public void DoWork(Action work)
         {
-            //Activate all locks before doing any work
-            foreach (object objLock in _locks)
-                Monitor.Enter(objLock);
+            var locksEnumerable = safeGetLocks();
+            int locksEntered = 0;
+
+            bool allLocksEntered = false;
 
             try
             {
+                //Activate all locks before doing any work
+                foreach (object objLock in locksEnumerable)
+                {
+                    Monitor.Enter(objLock);
+                    locksEntered++;
+                }
+
+                allLocksEntered = true;
                 work.Invoke();
             }
             finally
             {
+                if (!allLocksEntered)
+                    UtilityLogger.LogWarning("LogUtils was unable to enter all locks. Work was aborted");
+
+                var locksEnumerator = locksEnumerable.GetEnumerator();
+
                 //Release them when work is finished
-                foreach (object objLock in _locks)
-                    Monitor.Exit(objLock);
+                while (locksEntered != 0)
+                {
+                    locksEntered--;
+                    locksEnumerator.MoveNext();
+                    Monitor.Exit(locksEnumerator.Current);
+                }
             }
         }
+
+        private IEnumerable<object> safeGetLocks() => (UseEnumerableWrapper ? _locks.ToArray() : _locks).Where(o => o != null);
     }
 }
