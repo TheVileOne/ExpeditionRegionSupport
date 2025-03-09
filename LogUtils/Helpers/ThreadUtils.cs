868c455f6ddd24be1f42187a3b6438dfe55d29d0
Fluffball <xeolite@yahoo.com>
2025-03-08 21:35:07 -0500

    Use negative value as thread id default until a proper value is assigned

--- LogUtils/Helpers/ThreadUtils.cs
+++ LogUtils/Helpers/ThreadUtils.cs
@@ -4,7 +4,7 @@ namespace LogUtils.Helpers
 {
     public static class ThreadUtils
     {
-        public static int MainThreadID; 
+        public static int MainThreadID = -1;
 
         public static bool IsRunningOnMainThread => MainThreadID == Environment.CurrentManagedThreadId;
 
@@ -12,8 +12,15 @@ namespace LogUtils.Helpers
         {
             if (IsRunningOnMainThread) return true;
 
-            UtilityLogger.Logger.LogDebug("Assert failed: currently not on main thread");
-            UtilityLogger.Logger.LogDebug($"ThreadInfo: Id [{Environment.CurrentManagedThreadId}] Source [{context}]");
+            if (MainThreadID == -1)
+            {
+                UtilityLogger.Logger.LogDebug("Assert failed: Unable to determine main thread ID");
+            }
+            else
+            {
+                UtilityLogger.Logger.LogDebug("Assert failed: Currently not on main thread");
+                UtilityLogger.Logger.LogDebug($"ThreadInfo: Id [{Environment.CurrentManagedThreadId}] Source [{context}]");
+            }
             return false;
         }
     }
