e4aee648152253a739d8cf33e743b716979bbd95
Fluffball <xeolite@yahoo.com>
2025-06-29 18:22:54 -0400

    Move format hook to separate class

--- LogUtils/GameHooks.cs
+++ LogUtils/GameHooks.cs
@@ -13,8 +13,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 using UnityEngine;
 
 namespace LogUtils
@@ -30,10 +28,7 @@ namespace LogUtils
         {
             try
             {
-                ILHook hook = null;
-                BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
-                managedHooks.Add(hook = new ILHook(typeof(StringBuilder).GetMethod("AppendFormatHelper", flags), StringBuilder_AppendFormatHelper));
-
+                managedHooks.Add(FormatHook.Create());
                 Apply();
             }
             catch (Exception ex)
@@ -241,102 +236,6 @@ namespace LogUtils
             if (self.manager?.currentMainLoop == self)
                 UtilityEvents.OnNewUpdateSynced?.Invoke(self, EventArgs.Empty);
             orig(self);
-        }
-
-        private static void StringBuilder_AppendFormatHelper(ILContext il)
-        {
-            ILCursor cursor = new ILCursor(il);
-
-            //Match the third char comparison check - that's where the starting curly brace code is handled
-            cursor.GotoNext(MoveType.After, x => x.MatchBneUn(out _));
-            cursor.GotoNext(MoveType.After, x => x.MatchBneUn(out _));
-            cursor.GotoNext(MoveType.After, x => x.MatchBneUn(out _));
-
-            cursor.Emit(OpCodes.Ldloc_3); //ICustomFormatter
-            cursor.Emit(OpCodes.Ldloc_0); //Current index of format string
-            cursor.EmitDelegate(formatPlaceholderStart);
-
-            cursor.GotoNext(MoveType.After, x => x.MatchLdarga(3),       //args array is accessed
-                                            x => x.MatchLdloc(out _),    //Array indexer
-                                            x => x.Match(OpCodes.Call)); //Argument at the provided index is fetched from the args array
-
-            //Handle the char position of the right-most curly brace
-            cursor.Emit(OpCodes.Ldloc_3); //ICustomFormatter
-            cursor.Emit(OpCodes.Ldarg_2); //Format string
-            cursor.Emit(OpCodes.Ldloc, 4); //Argument index
-            cursor.Emit(OpCodes.Ldloc, 6); //Argument comma value
-            cursor.Emit(OpCodes.Ldloc_0); //Current index within format string
-            cursor.EmitDelegate((object formatArgument, ICustomFormatter formatter, string format, int argIndex, int commaArg, int formatIndex) =>
-            {
-                var provider = formatter as IColorFormatProvider;
-
-                if (provider != null)
-                {
-                    var formatData = provider.GetData();
-                    var placeholderData = formatData.CurrentPlaceholder;
-
-                    placeholderData.ArgumentIndex = argIndex;
-                    placeholderData.CommaArgument = commaArg;
-
-                    int placeholderStart = placeholderData.Position;
-                    int placeholderLength = formatIndex - placeholderStart;
-
-                    placeholderData.Format = format.Substring(placeholderStart, placeholderLength);
-
-                    //UtilityLogger.Log("PLACEHOLDER FORMAT: " + placeholderData.Format);
-
-                    if (formatArgument is Color)
-                        formatArgument = new ColorPlaceholder((Color)formatArgument, placeholderData);
-
-                    placeholderData.Argument = formatArgument;
-
-                    //Replaced original struct with the more updated copy
-                    formatData.CurrentPlaceholder = placeholderData;
-                }
-                return formatArgument;
-            });
-
-            cursor.GotoNext(MoveType.After, x => x.MatchStloc(11)); //Assignment of local variable responsible for padding spaces
-
-            ILLabel branchTarget = il.DefineLabel();
-
-            //Put it back on the stack, and check that is not 0
-            cursor.Emit(OpCodes.Ldloc, 11);
-            cursor.Emit(OpCodes.Brfalse, branchTarget);
-
-            //When value is not 0, we need to check if we dealing with the right formatter object
-            cursor.Emit(OpCodes.Ldloc, 11);
-            cursor.Emit(OpCodes.Ldloc_3);
-            cursor.EmitDelegate((int value, ICustomFormatter formatter) =>
-            {
-                var provider = formatter as IColorFormatProvider;
-
-                if (provider != null)
-                {
-                    var formatData = provider.GetData();
-                    var placeholderData = formatData.CurrentPlaceholder;
-
-                    //The padding syntax is being borrowed - this will prevent any padding from being assigned when we are working with a color argument
-                    if (placeholderData.Argument is ColorPlaceholder)
-                        return 0;
-                }
-                return value;
-            });
-            cursor.Emit(OpCodes.Stloc, 11); //Update padding value
-            cursor.MarkLabel(branchTarget);
-
-            void formatPlaceholderStart(ICustomFormatter formatter, int index)
-            {
-                //We only need to touch CWT data in the context of dealing with a IColorFormatProvider implementation
-                var provider = formatter as IColorFormatProvider;
-
-                if (provider != null)
-                {
-                    //UtilityLogger.Log("Placeholder start");
-                    var data = provider.GetData();
-                    data.CurrentPlaceholder.Position = index;
-                }
-            }
         }
 
         private static void RainWorld_HandleLog(On.RainWorld.orig_HandleLog orig, RainWorld self, string logString, string stackTrace, LogType category)
