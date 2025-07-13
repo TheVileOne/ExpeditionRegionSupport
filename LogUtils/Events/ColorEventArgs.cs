2e98d0bc68b7c88a7b890830b1f4c4ff8f3e26f6
Fluffball <xeolite@yahoo.com>
2025-07-13 17:21:48 -0400

    Add missing data class

---
+++ LogUtils/Events/ColorEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace LogUtils.Events
+{
+    public class ColorEventArgs : EventArgs
+    {
+        public Color Color { get; }
+
+        public ColorEventArgs(Color color)
+        {
+            Color = color;
+        }
+    }
+}
