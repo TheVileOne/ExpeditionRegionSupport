964b8c6435165d0705bef6ef14bd33b594d24448
Fluffball <xeolite@yahoo.com>
2025-08-28 17:56:48 -0400

    Attributes for reflection loaded LogRule support

---
+++ LogUtils/MissingAttributeException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LogUtils
+{
+    /// <summary>
+    /// Exception is thrown when an required attribute is not found
+    /// </summary>
+    public sealed class MissingAttributeException : Exception;
+}
