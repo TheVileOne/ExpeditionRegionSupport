67583dbaeb1a754798fc4b849c4a15c4c879669b
Fluffball <xeolite@yahoo.com>
2026-02-11 21:30:36 -0500

    Replace initial dialog design with one that offers more group handling options

---
+++ LogUtils/LogGroupTransferDialog.cs
@@ -0,0 +1,125 @@
+﻿using LogUtils.Enums;
+using LogUtils.Helpers;
+using Menu;
+using System;
+using UnityEngine;
+
+namespace LogUtils
+{
+    public class LogGroupTransferDialog : Dialog
+    {
+        internal const string DESCRIPTION = "A log group change has been detected that requires your input.";
+
+        internal const float PADDING = 14f;
+
+        private static ProcessManager currentProcess => RainWorldInfo.RainWorld.processManager;
+
+        private LogGroupID groupID;
+        private string destinationPath;
+
+        private RadioButtonGroup dialogOptions;
+
+        public LogGroupTransferDialog(LogGroupID groupID, string destinationPath) : base(DESCRIPTION, currentProcess)
+        {
+            this.groupID = groupID;
+            this.destinationPath = destinationPath;
+
+            Vector2 currentPosition = new Vector2(descriptionLabel.pos.x, descriptionLabel.pos.y + descriptionLabel.size.y);
+
+            currentPosition.y += PADDING * 2;
+            addDetails(ref currentPosition);
+
+            currentPosition.y += PADDING;
+            addOptions(ref currentPosition);
+
+            currentPosition.y += PADDING;
+            SimpleButton acceptButton = new SimpleButton(this, dialogPage, Translate("Accept"), "ACCEPT",  currentPosition, new Vector2(110f, 30f));
+            dialogPage.subObjects.Add(acceptButton);
+        }
+
+        private void addDetails(ref Vector2 position)
+        {
+            Vector2 labelSize = new Vector2(180f, 24f);
+
+            string[] detailLabels =
+            {
+                //Group name
+                "GROUP NAME      : " + groupID.Value,
+                //Group path
+                "CURRENT PATH    : " + groupID.Properties.CurrentFolderPath,
+                //Destination path
+                "DESTINATION PATH: " + destinationPath
+            };
+
+            for (int i = 0; i < detailLabels.Length; i++)
+            {
+                Vector2 labelPosition = new Vector2(position.x, position.y + (labelSize.y * i));
+                dialogPage.subObjects.Add(new MenuLabel(this, dialogPage, detailLabels[i], labelPosition, labelSize, false));
+            }
+            position.y += labelSize.y * detailLabels.Length - 1; //Account for label positions
+        }
+
+        private void addOptions(ref Vector2 position)
+        {
+            dialogOptions = new RadioButtonGroup(this, dialogPage, position);
+
+            //In all of these cases, the user path will be chosen as the new log group path even in the case the files/folders don't make it to the new path
+            dialogOptions.AddOption(textWidth: 80f, "Move entire group folder", "FOLDER_MOVE");
+            dialogOptions.AddOption(textWidth: 80f, "Move group files only", "FILE_MOVE");
+            dialogOptions.AddOption(textWidth: 80f, "Change group path only", "CHANGE_PATH");
+
+            dialogOptions.SetInitial("FOLDER_MOVE");
+            dialogPage.subObjects.Add(dialogOptions);
+        }
+
+        /// <summary>
+        /// Initiates transfer strategy based on user selected option
+        /// </summary>
+        /// <param name="sender">Event source object</param>
+        /// <param name="message">Case-sensitive identifier</param>
+        public override void Singal(MenuObject sender, string message)
+        {
+            if (message == "OK" || message == "ACCEPT")
+            {
+                Singal(dialogOptions, dialogOptions.Selected.IDString);
+                return;
+            }
+
+            try
+            {
+                switch (message)
+                {
+                    case "FOLDER_MOVE":
+                        LogGroup.MoveFolder(groupID, destinationPath);
+                        Dismiss();
+                        return;
+                    case "FILE_MOVE":
+                        LogGroup.MoveFiles(groupID, destinationPath);
+                        Dismiss();
+                        return;
+                    case "CHANGE_PATH":
+                        groupID.Properties.ChangePath(destinationPath, applyToMembers: true);
+                        Dismiss();
+                        return;
+                    default:
+                        base.Singal(sender, message);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                UtilityLogger.LogError("Failed to move group folder", ex);
+            }
+        }
+
+        public void Show()
+        {
+            currentProcess.ShowDialog(this);
+        }
+
+        public void Dismiss()
+        {
+            currentProcess.StopSideProcess(this);
+        }
+    }
+}
---
+++ LogUtils/RadioButtonGroup.cs
@@ -0,0 +1,134 @@
+﻿using Menu;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LogUtils
+{
+    public class RadioButtonGroup : MenuObject, CheckBox.IOwnCheckBox
+    {
+        public event Events.EventHandler<RadioButtonGroup, SelectionChangedEventArgs> SelectionChanged;
+
+        public CheckBox Selected { get; private set; }
+
+        /// <summary>
+        /// The option set by default (one option has to be)
+        /// </summary>
+        internal CheckBox DefaultOption { get; private set; }
+
+        /// <summary>
+        /// The height of a <see cref="CheckBox"/> sprite
+        /// </summary>
+        public const float OPTION_HEIGHT = 24f;
+
+        /// <summary>
+        /// The distance between listed options
+        /// </summary>
+        public const float OPTION_PADDING = 10f;
+
+        /// <summary>
+        /// The position of the <see cref="RadioButtonGroup"/> options.
+        /// </summary>
+        public readonly Vector2 Position;
+
+        internal List<CheckBox> Options = new List<CheckBox>();
+
+        /// <summary>
+        /// Contains options belonging to <see langword="this"/> instance
+        /// </summary>
+        public IReadOnlyList<CheckBox> OptionCollection => Options;
+
+        public RadioButtonGroup(Menu.Menu menu, MenuObject owner, Vector2 position) : base(menu, owner)
+        {
+            Position = position;
+        }
+
+        /// <summary>
+        /// Selects an option by its identifying string
+        /// </summary>
+        /// <param name="sender">Event source object</param>
+        /// <param name="message">Case-sensitive identifier</param>
+        public override void Singal(MenuObject sender, string message)
+        {
+            CheckBox found = Options.Find(o => o.IDString == message);
+
+            if (found != null)
+            {
+                found.Checked = true;
+                return;
+            }
+            base.Singal(sender, message);
+        }
+
+        /// <summary>
+        /// Adds a new <see cref="RadioButtonGroup"/> option
+        /// </summary>
+        /// <param name="textWidth">The hover range for this option</param>
+        /// <param name="displayText">The text that will show on this option's display label</param>
+        /// <param name="optionID">An identifying string for a <see cref="RadioButtonGroup"/> option; can be selectable through <see cref="MenuObject.Singal"/></param>
+        public void AddOption(float textWidth, string displayText, string optionID)
+        {
+            Vector2 optionPos = new Vector2(Position.x, Position.y + ((OPTION_HEIGHT + OPTION_PADDING) * Options.Count));
+            CheckBox option = new CheckBox(menu, this, this, optionPos, textWidth, displayText, optionID, textOnRight: true);
+
+            subObjects.Add(option);
+            Options.Add(option);
+        }
+
+        /// <summary>
+        /// Removes a <see cref="RadioButtonGroup"/> option with the specified identifying string
+        /// </summary>
+        /// <param name="optionID">An identifying string for a <see cref="RadioButtonGroup"/> option/param>
+        public void RemoveOption(string optionID)
+        {
+            CheckBox found = Options.Find(o => o.IDString == optionID);
+
+            if (found == null)
+            {
+                UtilityLogger.LogWarning("Option not found");
+                return;
+            }
+
+            subObjects.Remove(found);
+            Options.Remove(found);
+            if (found == Selected) //When removing a selected option, a new option must be selected
+                Selected = Options.Count > 0 ? Options[0] : null;
+        }
+
+        public void SetInitial(string optionID)
+        {
+            CheckBox found = Options.Find(o => o.IDString == optionID);
+
+            if (found == null)
+            {
+                UtilityLogger.LogWarning("Option not found");
+                return;
+            }
+            Selected = DefaultOption = found;
+        }
+
+        bool CheckBox.IOwnCheckBox.GetChecked(CheckBox box)
+        {
+            if (Selected == null)
+                throw new InvalidOperationException("No option is selected");
+
+            return Selected == box;
+        }
+
+        void CheckBox.IOwnCheckBox.SetChecked(CheckBox box, bool checkState)
+        {
+            if (checkState && box != Selected && Options.Contains(box))
+            {
+                CheckBox lastSelected = Selected;
+                Selected = box;
+                SelectionChanged?.Invoke(this, new SelectionChangedEventArgs(lastSelected, box));
+            }
+        }
+    }
+
+    public class SelectionChangedEventArgs(CheckBox lastSelection, CheckBox newSelection) : EventArgs
+    {
+        public CheckBox LastSelection = lastSelection;
+        public CheckBox NewSelection = newSelection;
+    }
+}
