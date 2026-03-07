9defa4315bf6332e856c407b81783865cd2c8114
Fluffball <xeolite@yahoo.com>
2026-03-06 19:11:58 -0500

    Fix lock context not applying before creation event is raised

--- LogUtils/Threading/CombinationLock.cs
+++ LogUtils/Threading/CombinationLock.cs
@@ -13,11 +13,33 @@ namespace LogUtils.Threading
         private bool workCompleted;
         private TaskFinalizer workFinalizer;
 
+        /// <summary>
+        /// Constructs a new <see cref="Lock"/> object
+        /// </summary>
+        /// <param name="groupProvider">A collection of objects to acquire locks from</param>
+        /// <exception cref="LockInvocationException">An exception was thrown during a lock monitoring event.</exception>
         public CombinationLock(IEnumerable<T> groupProvider) : this(groupProvider, "CombinationLock")
         {
         }
 
+        /// <summary>
+        /// Constructs a new <see cref="Lock"/> object
+        /// </summary>
+        /// <param name="groupProvider">A collection of objects to acquire locks from</param>
+        /// <param name="context">An object that identifies this instance</param>
+        /// <exception cref="LockInvocationException">An exception was thrown during a lock monitoring event.</exception>
         public CombinationLock(IEnumerable<T> groupProvider, object context) : base(context)
+        {
+            Items = new ReadOnlyCollection<T>(groupProvider.ToList());
+        }
+
+        /// <summary>
+        /// Constructs a new <see cref="CombinationLock{T}"/> object
+        /// </summary>
+        /// <param name="groupProvider">A collection of objects to acquire locks from</param>
+        /// <param name="contextProvider">A callback that returns a context on demand</param>
+        /// <exception cref="LockInvocationException">An exception was thrown during a lock monitoring event.</exception>
+        public CombinationLock(IEnumerable<T> groupProvider, ContextProvider contextProvider) : base(contextProvider)
         {
             Items = new ReadOnlyCollection<T>(groupProvider.ToList());
         }
