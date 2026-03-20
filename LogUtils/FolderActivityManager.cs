dab696061e63c696b3eea7a89d7f5d51eec45666
Fluffball <xeolite@yahoo.com>
2026-03-20 18:49:13 -0400

    Define and store inactive records

--- LogUtils/FolderActivityManager.cs
+++ LogUtils/FolderActivityManager.cs
@@ -8,23 +8,27 @@ namespace LogUtils
 {
     internal class FolderActivityManager
     {
-        private readonly ThreadLocal<List<ActivityRecord>> _activity = new ThreadLocal<List<ActivityRecord>>(true);
+        private readonly ThreadLocal<List<ActivityRecord>> _activeRecords = new ThreadLocal<List<ActivityRecord>>(true);
+        private readonly ThreadLocal<List<ActivityRecord>> _inactiveRecords = new ThreadLocal<List<ActivityRecord>>(true);
 
-        internal IEnumerable<ActivityRecord> ActiveMoves => _activity.Values.SelectMany(list => list);
-        internal IEnumerable<ActivityRecord> ActiveMovesThisThread => _activity.Value;
+        internal IEnumerable<ActivityRecord> ActiveMoves => _activeRecords.Values.SelectMany(list => list);
+        internal IEnumerable<ActivityRecord> ActiveMovesThisThread => _activeRecords.Value;
+        internal IEnumerable<ActivityRecord> InactiveMoves => _inactiveRecords.Values.SelectMany(list => list);
+        internal IEnumerable<ActivityRecord> InactiveMovesThisThread => _inactiveRecords.Value;
 
         internal ThreadSafeEvent<FolderActivityManager, ActivityRecord> OnRecordAdded = new ThreadSafeEvent<FolderActivityManager, ActivityRecord>();
         internal ThreadSafeEvent<FolderActivityManager, ActivityRecord> OnRecordRemoved = new ThreadSafeEvent<FolderActivityManager, ActivityRecord>();
 
         public FolderActivityManager()
         {
-            _activity.Value = new List<ActivityRecord>();
+            _activeRecords.Value = new List<ActivityRecord>();
+            _inactiveRecords.Value = new List<ActivityRecord>();
         }
 
         public ActivityRecord AddRecord(string sourcePath, string destinationPath)
         {
             ActivityRecord record;
-            _activity.Value.Add(record = new ActivityRecord()
+            _activeRecords.Value.Add(record = new ActivityRecord()
             {
                 SourcePath = sourcePath,
                 DestinationPath = destinationPath
@@ -33,15 +37,46 @@ namespace LogUtils
             return record;
         }
 
+        /// <summary>
+        /// Removes an active record associated with any thread
+        /// </summary>
         public bool RemoveRecordAnyThread(ActivityRecord record)
         {
             bool removed = false;
-            foreach (var entries in _activity.Values)
+            foreach (var entries in _activeRecords.Values)
             {
                 removed = entries.Remove(record);
                 if (removed)
                 {
                     OnRecordRemoved.Raise(this, record);
+                    break;
+                }
+            }
+            return removed;
+        }
+
+        /// <summary>
+        /// Changes an active record to an inactive record
+        /// </summary>
+        public void SetInactive(ActivityRecord record)
+        {
+            if (!RemoveRecordAnyThread(record))
+                UtilityLogger.LogWarning("Expected active move record");
+            _inactiveRecords.Value.Add(record);
+        }
+
+
+        /// <summary>
+        /// Removes an inactive record associated with any thread
+        /// </summary>
+        public bool RemoveInactiveAnyThread(ActivityRecord record)
+        {
+            bool removed = false;
+            foreach (var entries in _inactiveRecords.Values)
+            {
+                removed = entries.Remove(record);
+                if (removed)
+                {
                     break;
                 }
             }
