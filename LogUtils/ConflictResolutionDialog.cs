f8a306dba785f73c67c0d45537da5edf5b61f6e3
Fluffball <xeolite@yahoo.com>
2026-03-22 18:51:55 -0400

    Add design components

--- LogUtils/ConflictResolutionDialog.cs
+++ LogUtils/ConflictResolutionDialog.cs
@@ -1,6 +1,9 @@
-using LogUtils.Events;
+﻿using LogUtils.Events;
+using Menu;
+using Menu.Remix.MixedUI;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 using static LogUtils.FolderActivityManager;
@@ -10,7 +13,15 @@ namespace LogUtils
 {
     public class ConflictResolutionDialog : UtilityDialog
     {
-        internal const string DESCRIPTION = "Conflict resolution test";
+        internal const string DESCRIPTION = "Attempt to move files unable to continue. {0} Select resolution option to continue.";
+
+        internal const float PADDING_X = 90f;
+        internal const float PADDING_Y = 15f;
+
+        internal const float LABEL_HEIGHT = 24f;
+
+        private Vector2 detailsPosition;
+        private PositionedMenuObject detailsContainer;
 
         private readonly MergeEventHandler events;
         private readonly MergeHistory history;
@@ -19,11 +30,50 @@ namespace LogUtils
 
         private IEnumerator<MergeRecord> skippedEntries;
 
-        public ConflictResolutionDialog(MergeHistory history, MergeEventHandler mergeEvents) : base(DESCRIPTION, new Vector2(200, 200), RainWorldInfo.RainWorld.processManager)
+        public ConflictResolutionDialog(MergeHistory history, MergeEventHandler mergeEvents) : base(createDescription(history), calculateSize(history), RainWorldInfo.RainWorld.processManager)
         {
             this.events = mergeEvents;
             this.handler = new ConflictResolutionHandler();
             this.history = history;
+
+            //Old value: 97.5f
+            const float DESCRIPTION_LABEL_OFFSET = 117.5f; //Defines the spacing between the top of the screen (higher value means label draws higher on the screen)
+            const float DETAILS_LABEL_OFFSET = 52.51f;     //Defines the spacing between the description label and the detail labels
+
+            descriptionLabel.pos.y = pos.y + DESCRIPTION_LABEL_OFFSET;
+
+            //UtilityLogger.Log("Rect position " + roundedRect.pos);
+            //UtilityLogger.Log("Rect size " + roundedRect.size);
+            //UtilityLogger.Log("Label position " + descriptionLabel.pos);
+
+            //Set initial position to top-left position from bottom-left aligned position
+            Vector2 initialPosition = new Vector2(roundedRect.pos.x, roundedRect.pos.y + roundedRect.size.y);
+            Vector2 currentPosition = new Vector2(initialPosition.x, descriptionLabel.pos.y + DETAILS_LABEL_OFFSET);
+
+            detailsPosition = currentPosition;
+            updateDetails();
+            currentPosition.y -= LABEL_HEIGHT * detailsContainer.subObjects.Count + 1; //Account for label positions
+            currentPosition.y -= PADDING_Y * 6; //Set some extra space between the options and the info section
+
+            Vector2 buttonPosition;
+            Vector2 buttonSize = new Vector2(150f, 30f);
+            float buttonSpacing = PADDING_X;
+
+            //TODO: Figure out why this assignment does not work
+            float expectedTotalButtonsWidth = (buttonSize.x * 4) + (buttonSpacing * 3);
+            float buttonPadding = 160f; //(roundedRect.size.x - expectedTotalButtonsWidth) / 2;
+
+            buttonPosition = new Vector2(buttonPadding + currentPosition.x + (buttonSize.x / 2), currentPosition.y);
+            dialogPage.subObjects.Add(new SimpleButton(this, dialogPage, Translate("Overwrite"), DialogOption.OVERWRITE, buttonPosition, buttonSize));
+
+            buttonPosition = new Vector2(buttonPosition.x + (buttonSize.x + buttonSpacing) - (buttonSize.x / 2), currentPosition.y);
+            dialogPage.subObjects.Add(new SimpleButton(this, dialogPage, Translate("Keep Both"), DialogOption.KEEP_BOTH, buttonPosition, buttonSize));
+
+            buttonPosition = new Vector2(buttonPosition.x + (buttonSize.x + buttonSpacing) - (buttonSize.x / 2), currentPosition.y);
+            dialogPage.subObjects.Add(new SimpleButton(this, dialogPage, Translate("Skip For Now"), DialogOption.SKIP, buttonPosition, buttonSize));
+
+            buttonPosition = new Vector2(buttonPosition.x + (buttonSize.x + buttonSpacing) - (buttonSize.x / 2), currentPosition.y);
+            dialogPage.subObjects.Add(new SimpleButton(this, dialogPage, Translate("Cancel"), DialogOption.CANCEL, buttonPosition, buttonSize));
         }
 
         /// <summary>
@@ -72,6 +122,43 @@ namespace LogUtils
                 dialog.Show();
                 UtilityEvents.OnSetupPeriodReached -= scheduledEvent;
             }
+        }
+
+
+        private static string createDescription(MergeHistory history)
+        {
+            switch (history.Conflicts.Count)
+            {
+                case 0:
+                    return "ERROR: No conflicts detected. Please report this.";
+                case 1:
+                    return string.Format(DESCRIPTION, "Destination already has a file named " + Path.GetFileName(history.Conflicts.Peek().CurrentPath));
+                default:
+                    return string.Format(DESCRIPTION, $"Destination has {history.Conflicts.Count()} files with the same name.");
+            }
+        }
+
+        private static Vector2 calculateSize(MergeHistory history)
+        {
+            const int DETAIL_WIDTH_BASE = 90; //The current size of unchanging label text
+            const int MIN_WIDTH = 800;        //The minimum width of the dialog at any label size
+
+            float height, width;
+
+            height = 400.01f; //0.01 is important here (LABEL FIX)
+
+            float longestDetail = 0f;
+            foreach (MergeRecord record in history.Conflicts)
+            {
+                float detailWidth = Mathf.Max(LabelTest.GetWidth(record.CurrentPath), LabelTest.GetWidth(record.OriginalPath))
+                                  + DETAIL_WIDTH_BASE + (PADDING_X * 2); //Account for margins
+                if (detailWidth > longestDetail)
+                    longestDetail = detailWidth;
+            }
+
+            UtilityLogger.Logger.LogDebug($"Longest label is {longestDetail} units");
+            width = Mathf.Max(longestDetail, MIN_WIDTH) + 0.01f; //Clamp to the minimum allowed value
+            return new Vector2(width, height);
         }
 
         /// <summary>
@@ -123,7 +210,7 @@ namespace LogUtils
                     Dismiss();
                     return;
                 }
-                //TODO: Update info to reflect new conflict information
+                updateDetails();
             }
         }
 
@@ -150,6 +237,42 @@ namespace LogUtils
             else
             {
                 activeConflict = skippedEntries.Current;
+            }
+        }
+
+        private void updateDetails()
+        {
+            string[] detailLabels =
+            {
+                //Current path
+                "CURRENT:         \'{0}\'", //Spaces align with destination label
+                //Destination path
+                "DESTINATION: \'{0}\'",
+            };
+
+            if (detailsContainer == null)
+            {
+                detailsContainer = new MenuContainer(this, dialogPage, detailsPosition);
+                dialogPage.subObjects.Add(detailsContainer);
+
+                for (int i = 0; i < detailLabels.Length; i++)
+                {
+                    float labelOffsetY = i > 0 ? LABEL_HEIGHT * i : 0;
+
+                    Vector2 labelPosition = new Vector2(detailsPosition.x + PADDING_X, detailsPosition.y - labelOffsetY);
+                    MenuLabel label = new MenuLabel(this, dialogPage, string.Format(detailLabels[i], "N/A"), labelPosition, Vector2.zero, false);
+
+                    label.label.alignment = FLabelAlignment.Left;
+                    detailsContainer.subObjects.Add(label);
+                }
+            }
+            else
+            {
+                MenuLabel currentLabel = (MenuLabel)detailsContainer.subObjects[0];
+                MenuLabel destinationLabel = (MenuLabel)detailsContainer.subObjects[1];
+
+                currentLabel.text = string.Format(detailLabels[0], activeConflict.OriginalPath);
+                destinationLabel.text = string.Format(detailLabels[1], activeConflict.CurrentPath);
             }
         }
     }
